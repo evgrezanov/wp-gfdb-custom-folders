f2ea5794499fd9d713093c489544688dc1902adc
Evgeniy Rezanov <evgrezanov@gmail.com>
2020-12-26 12:26:39 +0700

    clear files

--- wp-gfdb-custom-folders.php
+++ wp-gfdb-custom-folders.php
@@ -4,11 +4,11 @@
  * Plugin Name: WP_GFDP_custom_folders
  * Description: Add-on for GFrorms Dropbox plugin. New users has his folder, and all the pictures uploaded by this user would be stored in his folder.
  * Plugin URI: https://github.com/evgrezanov/wp_gfdb_custom_folders
- * Version: 2.1
+ * Version: 2.1.1
  * Author: Evgeniy Rezanov
  * Author URI: https://github.com/evgrezanov/
  */
-define('GFDB_CUSTOM_FOLDERS_ADDON_VERSION', '2.1');
+define('GFDB_CUSTOM_FOLDERS_ADDON_VERSION', '2.1.1');
 
 add_action('gform_loaded', array('GFDB_Custom_Folders_AddOn_Bootstrap', 'load'), 5);
 class GFDB_Custom_Folders_AddOn_Bootstrap{
